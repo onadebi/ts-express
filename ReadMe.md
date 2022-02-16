7e8f97dc2fa3a58658472d057cb2e1479d225974
Onaefe <onaefe@onaxsys.com>
2022-02-16 20:33:19 +0100

    Readfile from ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+NodeJS app with TypeScript
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,34 @@
-import express, { Router } from "express";
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+
+async function ExtractReadMe(): Promise<string> {
+  let content = new Promise<string>(async (resolve, reject) => {
+    fs.readFile(
+        path.join(__dirname,'../../ReadMe.md'),
+      {
+        encoding: "utf-8",
+      },
+      (err, data: string) => {
+        if (err) {
+          return reject("Unable to read file");
+          console.log(err);
+        } else {
+          return resolve(data);
+          console.log(data);
+        }
+      }
+    );
+  });
+  return content;
+}
 
 const routes = express.Router();
 
-routes.get("/", (req, resp) => {
-  resp.send("Router response");
+routes.get("/", async (req, resp) => {
+  ExtractReadMe().then(data => resp.send(`Router response from ReadMe.md>> ${data}`))
+  .catch(err=> resp.status(404).send(`Router response: ${err}`));
 });
 
 routes.get("/err", (req, res) => {
